c53cad6c48b4b4a8082a5269d7eb7253fa7bfdf9
Obulpathi <obulpathi@gmail.com>
2016-11-10 17:42:04 -0800

    Edges

--- code.js
+++ code.js
@@ -26,43 +26,14 @@ var network = cytoscape({
         'target-arrow-color': '#61bffc',
         'transition-property': 'background-color, line-color, target-arrow-color',
         'transition-duration': '0.5s'
-      }),
-
-/*
-  elements: {
-      nodes: [
-        { data: { id: 'a' } },
-        { data: { id: 'b' } },
-        { data: { id: 'c' } },
-        { data: { id: 'd' } },
-        { data: { id: 'e' } }
-      ],
-
-      edges: [
-        { data: { id: 'a"e', weight: 1, source: 'a', target: 'e' } },
-        { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
-        { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-        { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
-        { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
-        { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
-        { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
-      ]
-    },
-
-  layout: {
-    name: 'breadthfirst',
-    directed: true,
-    roots: '#a',
-    padding: 10
-  }
-  */
+      })
 });
 
 
 // create nodes
 var size = [3, 4, 2]
-for (i = 0; i < size.length; i++) {
-  for (j = 0; j < size[0]; j++) {
+for (let i = 0; i < size.length; i++) {
+  for (let j = 0; j < size[0]; j++) {
     network.add({
       group: "nodes",
       data: {id: (i * 10 + j).toString(), weight: 100},
@@ -72,19 +43,17 @@ for (i = 0; i < size.length; i++) {
 }
 
 // create edges
-/*
-for (i = 0; i < size.slice(0, -1).entries(); i++) {
-  for (j = 0; j < size[0]; j++) {
-      for (k = 0; k < size[0]; k++) {
+iterator = size.slice(0, -1).entries();
+for (let e of iterator) {
+  for(let j = 0; j < e[1]; j++){
+    for(let k = 0; k < size[e[0]]; k++){
       network.add({
-        group: "nodes",
-        data: { weight: i * 10 + j },
-        position: { x: i, y: j }
+        group: "edges",
+        data: {source: (e[0] * 10 + j).toString(), target: ((e[0]+1) * 10 + k).toString()}
       });
     }
   }
 }
-*/
 
 /*
 var bfs = network.elements().bfs('#a', function(){}, true);
